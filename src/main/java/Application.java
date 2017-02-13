e94046386658241fe4f23245fa5f58275d9e6b1f
fnunezkanut <fnunezkanut@users.noreply.github.com>
2017-02-13 11:18:32 +0000

    43

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,4 @@
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,6 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableAutoConfiguration
 public class Application {
+
+	@Value("${framework.name}")
+	String name;
 
 	public static void main(String[] args) {
 
@@ -16,5 +20,11 @@ public class Application {
 	public String hello(){
 
 		return "hi";
+	}
+
+	@RequestMapping("/name")
+	public String name(){
+
+		return name;
 	}
 }
